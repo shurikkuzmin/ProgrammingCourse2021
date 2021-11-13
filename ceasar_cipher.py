a0b8da37baf66e71826521953d9d42978196854f
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2021-11-13 09:35:04 -0500

    Adding Caesar cipher.

---
+++ ceasar_cipher.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  6 10:09:33 2021
+
+@author: shurik
+"""
+import sys
+
+#shift = int(input("Please enter the shift for Ceasar cipher: "))
+
+numbers=[0,234,2343,324,3264233426,23462634]
+string_letters='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+letters = []
+for i in range(0, 26):
+    print(string_letters[i])
+#letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O'
+
