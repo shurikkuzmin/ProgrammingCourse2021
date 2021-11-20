b8395f67f06cc579720a22eac640c3cd312bfadc
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2021-11-20 10:33:12 -0500

    Ceasar cipher is progressed.

--- ceasar_cipher.py
+++ ceasar_cipher.py
@@ -10,7 +10,8 @@ import numpy
 
 shift = int(input("Please enter the shift for Ceasar cipher: "))
 
-numbers=[0,234,2343,324,3264233426,23462634]
+text = input("Please enter your text to encode: ")
+
 string_letters='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 letters = []
@@ -20,7 +21,25 @@ for i in range(0, 26):
 letters = numpy.array(letters)
 
 transformed_letters = numpy.roll(letters, shift)
-print(transformed_letters)
+
+
+transformed_text = ""
+for letter in text:
+    my_letter = letter
+    if letter.islower():
+        my_letter = letter.capitalize()
+    my_index = -1
+    for index in range(26):
+        if my_letter == letters[index]:
+            my_index = index
+            break
+        
+    if my_index != -1:
+        transformed_text = transformed_text + transformed_letters[my_index]
+    else:
+        transformed_text = transformed_text + letter
+print(text)
+print(transformed_text)
 #transformed_letters = 26*['A']
 
 #for i in range(shift,26):
