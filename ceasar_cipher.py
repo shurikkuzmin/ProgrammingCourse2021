7579d590a8310511cd3ac8131d4a7bec29afc4a7
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2021-12-04 10:35:01 -0500

    Finished Ceasar cipher.

--- ceasar_cipher.py
+++ ceasar_cipher.py
@@ -8,42 +8,41 @@ Created on Sat Nov  6 10:09:33 2021
 import sys
 import numpy
 
-shift = int(input("Please enter the shift for Ceasar cipher: "))
-
-text = input("Please enter your text to encode: ")
-
 string_letters='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
 letters = []
 for i in range(0, 26):
     letters.append(string_letters[i])
-
 letters = numpy.array(letters)
 
+shift = int(input("Please enter the shift for Ceasar cipher: "))
 transformed_letters = numpy.roll(letters, shift)
 
+def cryptoLine(line):
+    transformed_line = ""
+    for letter in line:
+        my_letter = letter
+        if letter.islower():
+            my_letter = letter.capitalize()
+        my_index = -1
+        for index in range(26):
+            if my_letter == letters[index]:
+                my_index = index
+                break
+            
+        if my_index != -1:
+            if letter.islower():
+                transformed_line = transformed_line + transformed_letters[my_index].lower() 
+            else:
+                transformed_line = transformed_line + transformed_letters[my_index]
+        else:
+            transformed_line = transformed_line + letter
+    return transformed_line
 
-transformed_text = ""
-for letter in text:
-    my_letter = letter
-    if letter.islower():
-        my_letter = letter.capitalize()
-    my_index = -1
-    for index in range(26):
-        if my_letter == letters[index]:
-            my_index = index
-            break
-        
-    if my_index != -1:
-        transformed_text = transformed_text + transformed_letters[my_index]
-    else:
-        transformed_text = transformed_text + letter
-print(text)
-print(transformed_text)
-#transformed_letters = 26*['A']
+f = open("dracula2.txt")
+f2 = open("dracula_decoded.txt","w")
 
-#for i in range(shift,26):
-#    transformed_letters[i] = letters[i-shift]
-
-#for i in range(0, shift):
-#    transformed_letters[i] = letters[26 - shift + i]
+for line in f.readlines():
+    f2.write(cryptoLine(line))
+f.close()
+f2.close()
+    
